95df62033f44e722dc13bdd3cb7c95a2614fea54
Jeroen Ooms <jeroenooms@gmail.com>
2022-10-25 13:13:16 +0200

    arrow 10 no longer supports backports

--- .github/workflows/bundle.sh
+++ .github/workflows/bundle.sh
@@ -28,7 +28,7 @@ for URL in $URLS; do
 done
 
 # Copy libs
-rm -Rf lib
+rm -Rf lib lib-4.9.3
 mkdir -p lib/{x64-ucrt,x64,i386}
 cp -fv ${OUTPUT}/ucrt64/lib/*.a lib/x64-ucrt/
 cp -fv ${OUTPUT}/mingw64/lib/*.a lib/x64/
@@ -38,41 +38,3 @@ cp -fv ${OUTPUT}/mingw32/lib/*.a lib/i386/
 rm -Rf include/*
 cp -Rf ${OUTPUT}/mingw64/include/arrow include/
 cp -Rf ${OUTPUT}/mingw64/include/parquet include/
-
-# Cleanup temporary dir
-rm -Rf ${OUTPUT}/*
-
-# Setup backports repo
-function finish {
-  echo "Restoring pacman.conf"
-  cp -f /etc/pacman.conf.bak /etc/pacman.conf
-  rm -f /etc/pacman.conf.bak
-  pacman -Scc --noconfirm
-  pacman -Syy
-}
-trap finish EXIT
-cp /etc/pacman.conf /etc/pacman.conf.bak
-curl -Ol 'https://raw.githubusercontent.com/r-windows/rtools-backports/master/pacman.conf'
-cp -f pacman.conf /etc/pacman.conf
-pacman -Scc --noconfirm
-pacman -Syy
-
-# Download backports
-backports=$(echo mingw-w64-{i686,x86_64}-{snappy,thrift,arrow})
-URLS=$(pacman -Spdd $backports --cache=$OUTPUT)
-for URL in $URLS; do
-  curl -OLs $URL
-  FILE=$(basename $URL)
-  echo "Extracting: $URL"
-  tar xf $FILE -C ${OUTPUT}
-  rm -f $FILE
-done
-
-# Copy libs
-rm -Rf lib-4.9.3
-mkdir -p lib-4.9.3/{i386,x64}
-cp -fv ${OUTPUT}/mingw32/lib/lib*.a lib-4.9.3/i386/
-cp -fv ${OUTPUT}/mingw64/lib/lib*.a lib-4.9.3/x64/
-
-# Cleanup temporary dir
-rm -Rf ${OUTPUT} pacman.conf
